26bac51b799534ea2998bcdde04d95ac0c4423af
Oyvind Fanebust <oyvind.fanebust@gmail.com>
2014-04-10 15:51:35 +0200

    Include dom manipulation in example

--- js/foo.js
+++ js/foo.js
@@ -1,3 +1,3 @@
 module.exports = function() {
-  return "Hello World!";
+  document.body.innerHTML = "Hello World!";
 };
--- test/fooSpec.js
+++ test/fooSpec.js
@@ -1,6 +1,7 @@
 var foo = require('../js/foo');
 describe("A suite", function() {
   it("contains spec with an expectation", function() {
-    expect(foo()).toBe("Hello World!");
+    foo();
+    expect(document.body.innerHTML).toBe("Hello World!");
   });
 });
